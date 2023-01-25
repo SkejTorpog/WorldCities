6493313ce26d56d47cc30db78863f088913b322e
SkejTorpog <adad>
2023-01-25 18:00:03 +0600

    Add project files.

---
+++ WorldCities.Tests/CitiesController_Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldCities.Data.Models;
+using WorldCities.Data;
+using WorldCities.Controllers;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorldCities.Tests
+{
+    public class CitiesController_Test
+    {
+        /// <summary>
+        /// Test the GetCity() method
+        /// </summary>
+        [Fact]
+        public async void GetCity()
+        {
+            #region Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "WorldCities")
+                .Options;
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Add(new City()
+                {
+                    Id = 1,
+                    CountryId = 1,
+                    Lat = 1,
+                    Lon = 1,
+                    Name = "TestCity1"
+                });
+                context.SaveChanges();
+            }
+            City city_existing = null;
+            City city_nonExisting = null;
+            #endregion
+
+            #region Act
+            using (var context = new ApplicationDbContext(options))
+            {
+                var controller = new CitiesController(context);
+                city_existing = (await controller.GetCity(1)).Value;
+                city_nonExisting = (await controller.GetCity(2)).Value;
+            }
+            #endregion
+
+            #region Assert
+            Assert.NotNull(city_existing);
+            Assert.Null(city_nonExisting);
+            #endregion
+        }
+    }
+}
---
+++ WorldCities/Data/ApiResult.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
+
+namespace WorldCities.Data
+{
+    public class ApiResult<T>
+    {
+        #region Properties
+        /// <summary>
+        /// The data result
+        /// </summary>
+        public List<T> Data { get; private set; }
+
+        /// <summary>
+        /// Zero-based index of current page.
+        /// </summary>
+        public int PageIndex { get; private set; }
+
+        /// <summary>
+        /// Number of items contained in each page.
+        /// </summary>
+        public int PageSize { get; private set; }
+
+        /// <summary>
+        /// Total items count
+        /// </summary>
+        public int TotalCount { get; private set; }
+
+        /// <summary>
+        /// Total pages count
+        /// </summary>
+        public int TotalPages { get; private set; }
+        
+        /// <summary>
+        /// Sorting Column name (or null if none set)
+        /// </summary>
+        public string SortColumn { get; set; }
+
+        /// <summary>
+        /// Sorting Order ("ASC", "DESC" or null if none set)
+        /// </summary>
+        public string SortOrder { get; set; }
+
+        /// <summary>
+        /// Filter Column name (or null if none set)
+        /// </summary>
+        public string FilterColumn { get; set; }
+
+        /// <summary>
+        /// Filter Query string
+        /// (to be used within the given FilterColumn)
+        /// </summary>
+        public string FilterQuery { get; set; }
+
+        /// <summary>
+        /// TRUE if the current page has a previous page,
+        /// FALSE otherwise.
+        /// </summary>
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 0);
+            }
+        }
+
+        /// <summary>
+        /// TRUE if the current page has a next page, FALSE otherwise.
+        /// </summary>
+        public bool HasNextPage
+        {
+            get
+            {
+                return ((PageIndex + 1) < TotalPages);
+            }
+        }
+        #endregion
+
+        private ApiResult(
+            List<T> data,
+            int count,
+            int pageIndex,
+            int pageSize,
+            string sortColumn,
+            string sortOrder,
+            string filterColumn,
+            string filterQuery)
+        {
+            Data = data;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalCount = count;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            SortColumn = sortColumn;
+            SortOrder = sortOrder;
+            FilterColumn = filterColumn;
+            FilterQuery = filterQuery;
+        }
+
+        #region Methods
+        public static async Task<ApiResult<T>> CreateAsync(
+            IQueryable<T> source,
+            int pageIndex,
+            int pageSize,
+            string sortColumn = null,
+            string sortOrder = null,
+            string filterColumn = null,
+            string filterQuery = null)
+        {
+            if (!string.IsNullOrEmpty(filterColumn)
+                && !string.IsNullOrEmpty(filterQuery)
+                && IsValidProperty(filterColumn ))
+            {
+                source = source.Where(
+                    $"{filterColumn}.Contains(@0)",filterQuery);
+            }
+
+            var count = await source.CountAsync();
+
+            if (!string.IsNullOrEmpty(sortColumn)
+                && IsValidProperty(sortColumn))
+            {
+                sortOrder = !string.IsNullOrEmpty(sortOrder)
+                    && sortOrder.ToUpper() == "ASC"
+                    ? "ASC"
+                    : "DESC";
+                source = source.OrderBy(
+                    string.Format("{0} {1}",
+                    sortColumn, sortOrder));
+            }
+
+            // retrieve the SQL query (for debug purposes)
+            var sql = source.ToParametrizedSql();
+            // берутся ли тут данные из БД
+            // либо только при методе toListAsync?
+            source = source
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize);
+
+            var data = await source.ToListAsync();
+
+            return new ApiResult<T>(
+                data,
+                count,
+                pageIndex,
+                pageSize,
+                sortColumn,
+                sortOrder,
+                filterColumn,
+                filterQuery);
+        }
+
+
+        public static bool IsValidProperty(string propertyName,
+            bool throwExceptionIfNotFound = true)
+        {
+            var prop = typeof(T).GetProperty(
+                propertyName, BindingFlags.IgnoreCase |
+                BindingFlags.Public |
+                BindingFlags.Instance);
+            if (prop == null && throwExceptionIfNotFound)
+            {
+                throw new NotSupportedException(
+                    string.Format("ERROR: Property '{0}' does not exist.",
+                    propertyName));
+            }
+            return prop != null;
+        }
+        #endregion
+    }
+}
